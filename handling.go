70b45a7f57fb249e6e25ac4b8029420cc64ef4ac
Jesaya Fernando Napitupulu <jesayafernando9@gmail.com>
2023-08-26 11:42:50 +0700

    Clear the statement if-else for handling a pages

--- handling.go
+++ handling.go
@@ -1,14 +1,17 @@
 package main
 
+var (
+	pageSize = 500
+)
+
 func indexPageNumberCounter(getProjectSearchPage ProjectSearchPage) int {
-	if getProjectSearchPage.Paging.Total%500 == 0 && getProjectSearchPage.Paging.Total > 500 {
-		return getProjectSearchPage.Paging.Total / 500
-		// fmt.Println("a")
-	} else if getProjectSearchPage.Paging.Total%500 >= 0 && getProjectSearchPage.Paging.Total > 500 {
-		return (getProjectSearchPage.Paging.Total / 500) + 1
-		// fmt.Println("b")
-	} else {
-		return 1
-		// fmt.Println("c")
+	pages := getProjectSearchPage.Paging.Total / pageSize
+	if getProjectSearchPage.Paging.Total%pageSize > 0 {
+		pages++
 	}
+	if pages < 1 {
+		pages = 1
+	}
+	return pages
+
 }
